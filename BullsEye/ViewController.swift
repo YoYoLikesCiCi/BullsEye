e0274e73c6f3a827d63168eed86e9b5bc97c2576
YoYoLikesCiCi <YoYoLikesCiCi@gmail.com>
2019-05-10 21:00:15 +0800

    everything before the new look have been done

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         
         super.viewDidLoad()
         
-        startNewRound()
+        startNewGame()
         // Do any additional setup after loading the view.
     }
     
@@ -37,35 +37,63 @@ class ViewController: UIViewController {
         roundLabel.text = String(round)
     }
     
+    func startNewGame(){
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
+    @IBAction func startOver(){
+        startNewGame()
+    }
     @IBOutlet weak var slider:UISlider!
     @IBOutlet weak var targetLabel:UILabel!
     @IBOutlet weak var scoreLabel:UILabel!
     @IBOutlet weak var roundLabel:UILabel!
-    //显示当前分数
-    @IBAction func showAlert(){
-        var difference: Int
-        difference = abs(targetValue - currentValue)
-        
-        let points = 100 - difference
-        score += points
-        
-        let message = "You scored \(points) points"
-        
-        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
-        
-        alert.addAction(action)
-        
-        present(alert, animated: true, completion: nil)
-        
-        startNewRound()
-    }
-    
     
     //事件slider变化时
     @IBAction func sliderMoved(_ slider: UISlider){
         currentValue = lroundf(slider.value)
     }
+    
+    //显示当前分数
+    @IBAction func showAlert(){
+        var difference: Int
+        difference = abs(targetValue - currentValue)
+        
+        var points = 100 - difference
+        
+        
+        let title: String
+        if difference == 0{
+            title = "Perfect!"
+            points += 100
+        }else if difference < 5{
+            title = "You almost had it!"
+            if difference == 1{
+                points += 50
+            }
+        }else if difference < 10{
+            title = "Pretty good!"
+        }else{
+            title = "Not even close..."
+        }
+        
+        score += points
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: {_ in self.startNewRound()})
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+//        startNewRound()
+    }
+    
+    
+    
 }
 
