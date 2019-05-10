ba0ab38f651026a31dbbfc66f5cd6daacf6cd875
YoYoLikesCiCi <YoYoLikesCiCi@gmail.com>
2019-05-10 11:17:25 +0800

    add random number

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,15 +11,24 @@ import UIKit
 class ViewController: UIViewController {
 
     var currentValue: Int = 0
+    var targetValut = 0
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        targetValut = Int.random(in: 1...100)  //产生随机数
+        
+        currentValue = lroundf(slider.value)
         // Do any additional setup after loading the view.
     }
     
     
+    @IBOutlet weak var slider:UISlider!
+    
+    
+    //显示当前f分数
     @IBAction func showAlert(){
-        let message = "The value of the slider is:\(currentValue)"
+        let message = "The value of the slider is:\(currentValue)" + "\nThe target value is \(targetValut)"
         
         let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
         
@@ -30,7 +39,7 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    
+    //事件slider变化时
     @IBAction func sliderMoved(_ slider: UISlider){
         currentValue = lroundf(slider.value)
     }
