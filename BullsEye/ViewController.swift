f41756ed9f72ba48bbae7fa5f20d849873be54f8
YoYoLikesCiCi <YoYoLikesCiCi@gmail.com>
2019-05-10 10:06:48 +0800

    联系slider与hit me

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,23 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    
     @IBAction func showAlert(){
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
+        let message = "The value of the slider is:\(currentValue)"
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
     }
     
+    
     @IBAction func sliderMoved(_ slider: UISlider){
-        print("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
     }
 }
 
